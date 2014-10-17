d19b7fa130e83205639ae4b5b4fd0fcb04c20183
Felype Tabosa <felypeftc@yahoo.com.br>
2014-10-16 21:06:01 -0300

    Criei o arquivo pom

--- SimpleJspServletDB/src/com/daniel/controller/UserController.java
+++ SimpleJspServletDB/src/com/daniel/controller/UserController.java
@@ -14,6 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 import com.daniel.dao.UserDao;
 import com.daniel.model.User;
 
+
 public class UserController extends HttpServlet {
     private static final long serialVersionUID = 1L;
     private static String INSERT_OR_EDIT = "/user.jsp";
